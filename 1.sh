027f67df6cb3f6d4923dcd510a18c9ff2ef148ef
临晨 <1930488504@qq.com>
2024-07-31 02:10:41 +0000

    update 1.sh.
    Signed-off-by: 临晨 <1930488504@qq.com>

--- 1.sh
+++ 1.sh
@@ -2,10 +2,8 @@
 path_top=$(pwd)
 echo "最初路径：$path_top"
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo/'
-
 # SYNC ANDROID VENDOR CODE
 dir1="android_vendor"
-
 if [ ! -d "$dir1" ]; then
 # 路径不存在，使用 mkdir 命令创建目录
 # -p 选项确保即使目录已经存在也不会报错
@@ -14,7 +12,6 @@ if [ ! -d "$dir1" ]; then
 else
     echo "目录 '$dir1' 已存在。"
 fi
-
 cd $dir1
 echo "当前repo==$dir1"
 echo "当前工作目录：$(pwd)"
