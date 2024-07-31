5395d5ba0cb9403bb93e26eba45c6f52d2e60792
临晨 <1930488504@qq.com>
2024-07-31 02:11:16 +0000

    update 1.sh.
    Signed-off-by: 临晨 <1930488504@qq.com>

--- 1.sh
+++ 1.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
 path_top=$(pwd)
 echo "最初路径：$path_top"
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo/'
-# SYNC ANDROID VENDOR CODE
 dir1="android_vendor"
 if [ ! -d "$dir1" ]; then
-# 路径不存在，使用 mkdir 命令创建目录
-# -p 选项确保即使目录已经存在也不会报错
     mkdir -p "$dir1"
     echo "目录 '$dir1' 已创建。"
 else
