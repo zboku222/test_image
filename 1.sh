52b5c7ba7bbdc8eedd1ad62027e237436a399184
临晨 <1930488504@qq.com>
2024-07-31 02:14:06 +0000

    update 1.sh.
    Signed-off-by: 临晨 <1930488504@qq.com>

--- 1.sh
+++ 1.sh
@@ -2,14 +2,14 @@ path_top=$(pwd)
 echo "最初路径：$path_top"
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo/'
 dir1="android_vendor"
-if [ ! -d "$dir1" ]; then
+if [ ! -d "$dir1" ]; then  # 确保 then 前有空白字符
     mkdir -p "$dir1"
     echo "目录 '$dir1' 已创建。"
 else
     echo "目录 '$dir1' 已存在。"
 fi
-cd $dir1
+cd "$dir1"  # 确保变量使用双引号
 echo "当前repo==$dir1"
 echo "当前工作目录：$(pwd)"
-cd $path_top
-echo $EPO_URL
+cd "$path_top"  # 确保变量使用双引号
+echo "$REPO_URL"  # 变量名应为 REPO_URL，不是 EPO_URL
